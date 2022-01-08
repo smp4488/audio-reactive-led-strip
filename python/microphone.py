fdfedc636a47687d950a1de6b744913d11e8b54d
smp4488 <smp4488@aol.com>
2022-01-07 23:49:17 -0500

    trying to route audio

--- python/microphone.py
+++ python/microphone.py
@@ -15,10 +15,11 @@ def start_stream(callback):
     p = pyaudio.PyAudio()
 
     # print ( "Available devices:\n")
-    # for i in range(0, p.get_device_count()):
-    #     info = p.get_device_info_by_index(i)
-    #     print ( str(info["index"]) +  ": \t %s \n \t %s \n" % (info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
-    #     pass
+    for i in range(0, p.get_device_count()):
+        info = p.get_device_info_by_index(i)
+        print ( str(info["index"]) +  ": \t %s \n \t %s \n" % (info["name"], p.get_host_api_info_by_index(info["hostApi"])["name"]))
+        pass
+    
     for i in range(0, p.get_device_count()):
         print(i, p.get_device_info_by_index(i)['name'])
     device_id = 1
