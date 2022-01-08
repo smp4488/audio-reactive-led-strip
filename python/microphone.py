95bcd78ee05f0b107140a1333ea75617b5f45fac
smp4488 <smp4488@aol.com>
2022-01-07 23:27:57 -0500

    trying to route audio

--- python/microphone.py
+++ python/microphone.py
@@ -29,8 +29,8 @@ def start_stream(callback):
                     rate=config.MIC_RATE,
                     input=True,
                     frames_per_buffer=frames_per_buffer,
-                    input_device_index=2)
-                    # as_loopback=True)
+                    input_device_index=1,
+                    as_loopback=True)
     overflows = 0
     prev_ovf_time = time.time()
     while True:
