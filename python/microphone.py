54fececcd3213bb1960f26d6b2b52b3ae2c22c88
smp4488 <smp4488@aol.com>
2022-01-08 00:31:06 -0500

    trying to route audio

--- python/microphone.py
+++ python/microphone.py
@@ -22,7 +22,7 @@ def start_stream(callback):
 
     for i in range(0, p.get_device_count()):
         print(i, p.get_device_info_by_index(i)['name'])
-    device_id = 3
+    device_id = 2
     device_info = p.get_device_info_by_index(device_id)
     frames_per_buffer = int(config.MIC_RATE / config.FPS)
     stream = p.open(format=pyaudio.paInt16,
